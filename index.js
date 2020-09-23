5f30bb74f93c2d19e2ec96b1cbe4b10bf50483d4
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 21:07:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function takeANumber(katzDeliLine, name){
-  return  `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`
+  katzDeliLine.push(name)
+  
+  return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine){
