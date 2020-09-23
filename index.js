ddb8c992b2dfe95eeaa095dfb59e00a66212c008
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 21:18:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function nowServing(katzDeliLine){
     return string
   }
 }
+
+function currentLine(katzDeliLine){
+  let myString = `The line is currently: `
+  
+  for(let i=1; i<katzDeliLine.length - 1; i++){
+    myString += `${i}. ${katzDeliLine[i]} `
+  }
+  
+  if(katzDeliLine.length === 0){
+    return `The line is currently empty.`
+  }else{
+    return myString 
+  }
+}
